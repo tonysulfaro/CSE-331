c48f8f84c83b3f8a5f46734e1a2678900d28648c
Tony Sulfaro <tony116523@gmail.com>
2018-09-16 21:50:49 -0400

    Can insert items in order using insert method

--- Projects/project02/README.txt
+++ Projects/project02/README.txt
@@ -1,7 +1,7 @@
 Project 2
 
-Name:
-Time to Complete:
-Feedback:
+Name: Tony Sulfaro
+Time to Complete: 4 hours
+Feedback: Overall this was an interesting project that helped solidify recursion concepts. I wouldn't use recursion over iteration usually because of the function call overhead but it's good to know
 
 Sources Used:
--- Projects/project02/Recursion.py
+++ Projects/project02/Recursion.py
@@ -1,18 +1,33 @@
 """
 PROJECT 2 - Recursion
-Name:
-PID:
+Name: Tony Sulfaro
+PID: A52995491
 """
 
-from Project2.LinkedNode import LinkedNode
+from LinkedNode import LinkedNode
 
 
 def insert(value, node=None):
-    pass
+
+    if node is None or node.value > value:
+        return LinkedNode(value, None)
+
+    node.next_node = insert(value, node.next_node)
+
+    return node
 
 
 def string(node):
-    pass
+
+    output = ""
+
+    output += str(node.value)
+    output += ', '
+
+    if node.next_node is None:
+        return output[:-2]
+
+    return string(node.next_node)
 
 
 def reversed_string(node):
@@ -20,24 +35,78 @@ def reversed_string(node):
 
 
 def remove(value, node):
-    pass
+
+    if node.next_node.value == value:
+        node.next_node = node.next_node.next_node
+        return
+
+    return remove(value, node.next_node)
 
 
 def remove_all(value, node):
-    pass
+
+    if node.value == value:
+        print('hmm')
+
+    return remove_all(value, node.next_node)
 
 
 def search(value, node):
-    pass
+
+    if node.value == value:
+        return True
+
+    if node.next_node is not None:
+        return search(value, node.next_node)
+
+    return False
 
 
 def length(node):
-    pass
+
+    num = 0
+
+    if node is not None:
+        num += 1
+
+    if node.next_ndoe is not None:
+        return length(node.next_node)
+
+    return num
 
 
 def sum_all(node):
-    pass
+
+    if node is not None:
+        return node.value + sum_all(node.next_node)
+
+    return 0
 
 
 def count(value, node):
-    pass
+
+    num = 0
+
+    if node.value == value:
+        num += 1
+
+    return count(value, node.next_node)
+
+
+def main():
+
+    linked_node = insert(1, None)
+
+    linked_node = insert(2, linked_node)
+    linked_node = insert(3, linked_node)
+    linked_node = insert(4, linked_node)
+    linked_node = insert(5, linked_node)
+
+    node = linked_node
+    while node is not None:
+        print(node.value)
+        node = node.next_node
+
+
+if __name__ == '__main__':
+    main()
