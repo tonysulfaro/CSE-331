9066afa9d0b347d17098914c4cb8abfe70f14fcf
Tony Sulfaro <tony116523@gmail.com>
2018-09-27 11:40:12 -0400

    finished in class

--- Lecture/Lecture09-Queues/array_queue.py
+++ Lecture/Lecture09-Queues/array_queue.py
@@ -19,3 +19,38 @@ class ArrayQueue:
     def is_Empty(self):
         """Return true if list is empty"""
         return self.__size == 0
+
+    def _resize(self, cap):
+        old = self.__data
+        self.data = [None]*cap
+        walk = self.__front
+
+        for k in range(self.__size):
+            self.__data[k] = old[walk]  # normalization, set front to 0
+            walk = (1+walk) % len(old)
+
+        self.__front = 0
+
+    def enqueue(self, e):
+        if self.__size == len(self.__data):
+            self._resize(2*len(self.__data))
+
+        # find the next availible spot on that list
+        avail = (self.__front + self.__size) % len(self.__data)
+
+        # put data in right place
+        self.__data[avail] = e
+
+        # increment the size by one
+        self.__size += 1
+
+    def dequeue(self):
+        if self.is_Empty():
+            raise Empty('The queue is empty, cant do that')
+        answer = self.__data[self.__front]
+        self.__data[self.__front] = None  # remove the front, set to null
+        self.__front = (self.__front + 1) % len(self.__data)
+
+        # decrement the size
+        self.__size -= 1
+        return answer
