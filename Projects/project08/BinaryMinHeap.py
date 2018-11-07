b81e5e3b1c02ea31925785912cdbb24b1b107e4c
Tony Sulfaro <tony116523@gmail.com>
2018-11-07 10:16:52 -0500

    initial commit of project08 base files

---
+++ Projects/project08/BinaryMinHeap.py
@@ -0,0 +1,75 @@
+########################################
+# PROJECT: Binary Min Heap and Sort
+# Author:
+########################################
+
+class BinaryMinHeap:
+    # DO NOT MODIFY THIS CLASS #
+    def __init__(self):
+        """
+        Creates an empty hash table with a fixed capacity
+        """
+        self.table = []
+
+
+    def __eq__(self, other):
+        """
+        Equality comparison for heaps
+        :param other: Heap being compared to
+        :return: True if equal, False if not equal
+        """
+        if len(self.table) != len(other.table):
+            return False
+        for i in range(len(self.table)):
+            if self.table[i] != other.table[i]:
+                return False
+
+        return True
+
+    ###### COMPLETE THE FUNCTIONS BELOW ######
+
+    def __str__(self):
+        pass
+
+    def get_size(self):
+        pass
+
+    def parent(self, position):
+        pass
+
+    def left_child(self, position):
+        pass
+
+    def right_child(self, position):
+        pass
+
+    def has_left(self, position):
+        pass
+
+    def has_right(self, position):
+        pass
+
+    def find(self, value):
+        pass
+
+    def heap_push(self, value):
+        pass
+
+    def heap_pop(self, value):
+        pass
+
+    def pop_min(self):
+        pass
+
+    def swap(self, p1, p2):
+        pass
+
+    def percolate_up(self, position):
+        pass
+
+    def percolate_down(self, position):
+        pass
+
+
+def heap_sort(unsorted):
+    pass
